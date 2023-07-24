4a7b1f5c611502fea08dc4456cb3d8306468f0aa
willysalazar <willysal@gmail.com>
2023-07-24 14:32:17 -0300

    :tada: :sparkles: setup of project

---
+++ src/main/java/com/willysalazar/garagesaleapi/GaragesaleApiApplication.java
@@ -0,0 +1,13 @@
+package com.willysalazar.garagesaleapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GaragesaleApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GaragesaleApiApplication.class, args);
+	}
+
+}
