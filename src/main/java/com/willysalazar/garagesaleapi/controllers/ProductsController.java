31af4de423c26a18285573c213f55c58ec349f81
willysalazar <willysal@gmail.com>
2023-07-25 20:05:30 -0300

    :sparkles: add getAll products

--- src/main/java/com/willysalazar/garagesaleapi/controllers/ProductsController.java
+++ src/main/java/com/willysalazar/garagesaleapi/controllers/ProductsController.java
@@ -6,6 +6,10 @@ import com.willysalazar.garagesaleapi.services.ProductsService;
 import jakarta.validation.Valid;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -28,6 +32,11 @@ public class ProductsController {
         productsModel.setRegistrationDate(LocalDateTime.now(ZoneId.of("UTC")));
 
         return ResponseEntity.status(HttpStatus.CREATED).body(productsService.save(productsModel));
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<ProductsModel>> getProducts(@PageableDefault(page = 0, size = 10, sort = "id", direction = Sort.Direction.ASC) Pageable pageable){
+        return ResponseEntity.status(HttpStatus.OK).body(productsService.findAll(pageable));
     }
 
 }
